daf607cb53f2dd31ee82a6cccce92a4b7857336f
Yuva <yuva@codemancers.com>
2012-06-27 04:40:26 +0530

    add assets for precompiling

--- lib/wymeditor-rails/engine.rb
+++ lib/wymeditor-rails/engine.rb
@@ -1,5 +1,15 @@
 module WymeditorRails
   class Engine < ::Rails::Engine
     # this just lets rails know to load from vendor/assets
+
+    # Enabling assets precompiling
+    initializer :assets do |app|
+      app.config.assets.precompile +=
+        [
+         "wymeditor/lang/*",
+         "wymeditor/skins/**/*",
+         "wymeditor/iframe/**/*"
+        ]
+    end
   end
 end
